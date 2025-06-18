751cbd620d72e5cc1fbbaa746b451ced37705d62
Lule-M <miha.luka1006@gmail.com>
2025-06-18 23:30:50 +0200

    seed migrations

---
+++ FilmStudioManager/Migrations/20250618212823_AddGenreSeeders.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace FilmStudioManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGenreSeeders : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Genres",
+                columns: new[] { "GenreId", "GenreName" },
+                values: new object[,]
+                {
+                    { 1, "Action" },
+                    { 2, "Drama" },
+                    { 3, "Comedy" },
+                    { 4, "Sci-Fi" },
+                    { 5, "Horror" },
+                    { 6, "Western" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "GenreId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "GenreId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "GenreId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "GenreId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "GenreId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "GenreId",
+                keyValue: 6);
+        }
+    }
+}
