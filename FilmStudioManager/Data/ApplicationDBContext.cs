8b5eb077a91868c595e14798bb6d3a7a5f1c506c
Lule-M <miha.luka1006@gmail.com>
2025-06-18 23:27:57 +0200

    add seeder

--- FilmStudioManager/Data/ApplicationDBContext.cs
+++ FilmStudioManager/Data/ApplicationDBContext.cs
@@ -11,22 +11,31 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
     }
 
-protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<FilmEmployee>()
-                .HasKey(fe => new { fe.FilmId, fe.EmployeeId });
+        modelBuilder.Entity<FilmEmployee>()
+            .HasKey(fe => new { fe.FilmId, fe.EmployeeId });
 
-            modelBuilder.Entity<FilmEmployee>()
-                .HasOne<Film>()
-                .WithMany(f => f.FilmEmployees)
-                .HasForeignKey(fe => fe.FilmId);
+        modelBuilder.Entity<FilmEmployee>()
+            .HasOne<Film>()
+            .WithMany(f => f.FilmEmployees)
+            .HasForeignKey(fe => fe.FilmId);
 
-            modelBuilder.Entity<FilmEmployee>()
-                .HasOne<Employee>()
-                .WithMany(e => e.FilmEmployees)
-                .HasForeignKey(fe => fe.EmployeeId);
+        modelBuilder.Entity<FilmEmployee>()
+            .HasOne<Employee>()
+            .WithMany(e => e.FilmEmployees)
+            .HasForeignKey(fe => fe.EmployeeId);
+                
+        modelBuilder.Entity<Genre>().HasData(
+            new Genre { GenreId = 1, GenreName = "Action" },
+            new Genre { GenreId = 2, GenreName = "Drama" },
+            new Genre { GenreId = 3, GenreName = "Comedy" },
+            new Genre { GenreId = 4, GenreName = "Sci-Fi" },
+            new Genre { GenreId = 5, GenreName = "Horror" },
+            new Genre { GenreId = 6, GenreName = "Western" }
+            );
         }
 
     public DbSet<Genre> Genres { get; set; }
